f287bf63281724e1474a2494b2dd0fcd618894be
dhkoo <amainlog@gmail.com>
2024-06-28 14:16:20 +0900

    update: update health api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from starlette.middleware.cors import CORSMiddleware
+
+from app.schemas.base import ResponseBase
 
 
 app = FastAPI()
@@ -20,6 +22,14 @@ async def root():
     return {"message": "ok"}
 
 
-@app.get("/health")
-async def health():
-    return {"status": "ok"}
+@app.get(
+    "/health",
+    response_model=ResponseBase,
+    description="Health Check API",
+)
+async def health_check() -> ResponseBase:
+    return ResponseBase(
+        code=status.HTTP_200_OK,
+        message="Server Alive",
+        data={"status": "alive"},
+    )
