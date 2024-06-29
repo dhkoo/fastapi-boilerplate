94eb5a5aaca67baff33d85c02e54534d4e8f9aa8
dhkoo <amainlog@gmail.com>
2024-06-29 23:53:30 +0900

    chore: remove print code

--- app/databases/rdb.py
+++ app/databases/rdb.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from app.config import database_settings
 
-print(database_settings.db_url)
 engine = create_async_engine(database_settings.db_url)
 
 async_session = sessionmaker(
